71aed5e58e5531a8e40a8b2ee5da7bef717538d8
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-28 17:39:10 +0200

    first commit

---
+++ src/main/java/it/unisalento/pas/taxbe/TaxBeApplication.java
@@ -0,0 +1,13 @@
+package it.unisalento.pas.taxbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unisalento/pas/taxbe/TaxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisalento.pas.taxbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
