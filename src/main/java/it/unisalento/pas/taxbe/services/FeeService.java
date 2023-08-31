be757d47515a9801f3a0cd252dce253f12b2c243
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-31 22:02:06 +0200

    refactor del codice

--- src/main/java/it/unisalento/pas/taxbe/services/FeeService.java
+++ src/main/java/it/unisalento/pas/taxbe/services/FeeService.java
@@ -3,6 +3,7 @@ package it.unisalento.pas.taxbe.services;
 import it.unisalento.pas.taxbe.domains.Fee;
 import it.unisalento.pas.taxbe.domains.WasteStatistics;
 import it.unisalento.pas.taxbe.repositories.IFeeRepository;
+import it.unisalento.pas.taxbe.utils.FeeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -56,55 +57,8 @@ public class FeeService implements IFeeService{
     }
 
     @Override
-    public WasteStatistics getAllRegistredWasteByUserID(String userId, int year) {
-        List<Fee> feeList = feeRepository.findAllByUserId(userId);
-        WasteStatistics paidWastes = sumWastes(feeList);
-        paidWastes.setUserId(userId);
-        paidWastes.setYear(year);
-        return paidWastes;
-    }
-
-    @Override
     public ArrayList<Fee> getAllFees() {
         List<Fee> feeList = feeRepository.findAll();
         return new ArrayList<>(feeList);
     }
-
-    @Override
-    public WasteStatistics getNumberOfPaidWastesByUserID(String userId, int year) {
-        List<Fee> feeList = feeRepository.findAllByUserId(userId);
-        feeList.removeIf(tmpFee -> !tmpFee.getTimestamp().startsWith(String.valueOf(year)) || tmpFee.getPaid() != 1);
-
-        WasteStatistics paidWastes = sumWastes(feeList);
-        paidWastes.setUserId(userId);
-        paidWastes.setYear(year);
-        return paidWastes;
-    }
-
-    @Override
-    public WasteStatistics getNumberOfWastesByPayment(int year, int paid) {
-        List<Fee> feeList = feeRepository.findAll();
-        feeList.removeIf(tmpFee -> !tmpFee.getTimestamp().startsWith(String.valueOf(year)) || tmpFee.getPaid() != paid);
-
-        WasteStatistics paidWastes = sumWastes(feeList);
-        paidWastes.setYear(year);
-        return paidWastes;
-    }
-
-    private WasteStatistics sumWastes(List<Fee> feeList) {
-        WasteStatistics statistics = new WasteStatistics();
-
-        int sorted = 0;
-        int unsorted = 0;
-        for (Fee fee :
-                feeList) {
-            sorted = sorted + fee.getSortedWaste();
-            unsorted = unsorted + fee.getUnsortedWaste();
-        }
-
-        return statistics;
-    }
-
-
-
 }
